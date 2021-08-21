95ca9440bf46f7f39fa3fa5e85208866bde62a76
imprantu <rakinhasanbabu4@gmail.com>
2021-08-21 15:29:30 +0600

    fix minor bugs and validate promo code error handling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* 
+    Button handler for 8GB Unified Memory
+*/
+
 document.getElementById('btn-memory-default').addEventListener('click', function () {
     const extraMemoryCostText = document.getElementById('extra-memory-cost');
     extraMemoryCostText.innerText = 0;
@@ -5,6 +9,10 @@ document.getElementById('btn-memory-default').addEventListener('click', function
     const totalCost = updatePrice();
     totalCostText.innerText = totalCost;
 });
+
+/*
+    Button handler for 16GB Unified Memory
+*/
 
 document.getElementById('btn-memory-double').addEventListener('click', function () {
     const extraMemoryCostText = document.getElementById('extra-memory-cost');
@@ -14,6 +22,10 @@ document.getElementById('btn-memory-double').addEventListener('click', function 
     totalCostText.innerText = totalCost;
 });
 
+/*
+    Button handler for 256GB SSD Storage
+*/
+
 document.getElementById('btn-storage-default').addEventListener('click', function () {
     const extraStorageCostText = document.getElementById('extra-storage-cost');
     extraStorageCostText.innerText = 0;
@@ -21,6 +33,10 @@ document.getElementById('btn-storage-default').addEventListener('click', functio
     const totalCost = updatePrice();
     totalCostText.innerText = totalCost;
 });
+
+/*
+    Button handler for 512GB SSD Storage
+*/
 
 document.getElementById('btn-storage-double').addEventListener('click', function () {
     const extraStorageCostText = document.getElementById('extra-storage-cost');
@@ -30,6 +46,10 @@ document.getElementById('btn-storage-double').addEventListener('click', function
     totalCostText.innerText = totalCost;
 });
 
+/*
+    Button handler for 1TB SSD Storage
+*/
+
 document.getElementById('btn-storage-complete').addEventListener('click', function () {
     const extraStorageCostText = document.getElementById('extra-storage-cost');
     extraStorageCostText.innerText = 180;
@@ -37,6 +57,10 @@ document.getElementById('btn-storage-complete').addEventListener('click', functi
     const totalCost = updatePrice();
     totalCostText.innerText = totalCost;
 });
+
+/*
+    Button handler for free delivery
+*/
 
 document.getElementById('btn-delivery-free').addEventListener('click', function () {
     const deliveryChargeText = document.getElementById('delivery-cost');
@@ -46,6 +70,10 @@ document.getElementById('btn-delivery-free').addEventListener('click', function 
     totalCostText.innerText = totalCost;
 });
 
+/*
+    Button handler for express delivery
+*/
+
 document.getElementById('btn-delivery-express').addEventListener('click', function () {
     const deliveryChargeText = document.getElementById('delivery-cost');
     deliveryChargeText.innerText = 20;
@@ -53,6 +81,10 @@ document.getElementById('btn-delivery-express').addEventListener('click', functi
     const totalCost = updatePrice();
     totalCostText.innerText = totalCost;
 });
+
+/*
+    Function for calculate the total price
+*/
 
 function updatePrice() {
     const bestPriceText = document.getElementById('best-cost');
@@ -71,17 +103,24 @@ function updatePrice() {
     return totalCost;
 };
 
+/*
+    Button handler for promo code and total calculation after applied promo code and minor error handling
+*/
+
 document.getElementById('btn-promo-apply').addEventListener('click', function () {
     let totalPrice = updatePrice();
     const priceTotal = document.getElementById('price-total');
     const promoCodeField = document.getElementById('input-field-promo');
     const promoCode = promoCodeField.value;
-    if (promoCode == 'stevekaku') {
+    if (promoCode.toLowerCase() == 'stevekaku') {
         totalPrice = totalPrice - ((totalPrice * 20) / 100);
         priceTotal.innerText = totalPrice;
     }
+    else if (promoCode == '') {
+        alert('No promo code entered');
+    }
     else {
-        alert('Invalid Promo Code entered!');
+        alert('Invalid promo code entered, please enter correct promo code!');
     }
     promoCodeField.value = '';
 });
